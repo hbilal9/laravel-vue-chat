e28dcf356d2fe5c0578be6085c8a320e19185ab9
HBilal Khan Yousafzai <hbilal_9@hotmail.com>
2024-08-01 19:17:17 +0500

    Login view design created

--- frontent-vue/src/views/auth/LoginView.vue
+++ frontent-vue/src/views/auth/LoginView.vue
@@ -1,7 +1,61 @@
 <script setup lang="ts">
-import {} from 'vue'
+import { reactive } from 'vue'
+import Button from '@/components/ui/Button.vue'
+import TextInput from '@/components/ui/TextInput.vue'
+
+const user = reactive({
+  email: 'hbilal@gmail.com',
+  password: 'password'
+})
 </script>
 
 <template>
-  <div>Hello</div>
+  <div class="w-full h-full">
+    <div class="fixed inset-0 w-full h-full bg-secondary-50">
+      <div class="h-full w-auto flex justify-center text-secondary-600">
+        <div class="mx-5 sm:mx-0 md:w-full max-w-[30rem]">
+          <div class="w-full">
+            <div class="p-4 flex flex-col justify-center items-center text-center">
+              <!-- <img class="my-20 w-24" src="/images/logo.png" /> -->
+              <h1 class="text-3xl sm:text-2xl font-poppins md:text-3xl 2xl:text-3xl font-extrabold">
+                Welcome Back!
+              </h1>
+              <p class="text-gray-500">Enter the email and password you used to register</p>
+            </div>
+            <div class="text-secondary-700">
+              <form>
+                <TextInput
+                  key="email"
+                  v-model="user.email"
+                  label="Email"
+                  placeholder="Enter Email"
+                />
+                <TextInput
+                  key="password"
+                  v-model="user.password"
+                  type="password"
+                  label="Password"
+                  placeholder="Enter Password"
+                />
+                <small class="text-danger text-xs text-center block"></small>
+
+                <Button class="w-full h-12 rounded-lg mt-2"> Login </Button>
+              </form>
+              <div class="w-full flex items-center flex-col justify-center mt-8">
+                <div>
+                  <span>New here?</span>
+                  <router-link
+                    class="text-sm cursor-pointer select-none font-bold hover:underline"
+                    to="/register"
+                  >
+                    Create an account!
+                  </router-link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
