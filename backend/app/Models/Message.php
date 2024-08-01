d3dd703fa0be8e7163a0d181c49133e0b57c622b
HBilal Khan Yousafzai <hbilal_9@hotmail.com>
2024-08-01 12:58:23 +0500

    Message Model => fillable and relations created

--- backend/app/Models/Message.php
+++ backend/app/Models/Message.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['conversation_id', 'user_id', 'content', 'delivered', 'seen', 'seen_at'];
+
+    public function conversation()
+    {
+        return $this->belongsTo(Conversation::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
