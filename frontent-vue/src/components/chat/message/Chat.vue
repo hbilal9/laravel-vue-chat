07bcfaa07570501e12b75d349f50519c188a96c1
HBilal Khan Yousafzai <hbilal_9@hotmail.com>
2024-08-02 17:25:27 +0500

    typing - whipser added

--- frontent-vue/src/components/chat/message/Chat.vue
+++ frontent-vue/src/components/chat/message/Chat.vue
@@ -10,16 +10,34 @@ const conversationStore = useConversationStore()
 const store = useMessageStore()
 
 const message = ref('')
-
+const typing = ref(false)
+const timeout = ref(0)
 watch(
   () => conversationStore.selectedConversation,
   (conversation) => {
     store.fetchMessages(conversation?.id!)
-    echo.join(`conversation.${conversation?.id}`).listen('MessageSent', (e) => {
-      store.addMessage(e.message)
-    })
+    echo
+      .join(`conversation.${conversation?.id}`)
+      .listen('MessageSent', (e) => {
+        store.addMessage(e.message)
+      })
+      .listenForWhisper('typing', (e) => {
+        typing.value = e.typing
+        if (timeout.value) {
+          clearTimeout(timeout.value)
+        }
+        timeout.value = setTimeout(() => {
+          typing.value = false
+        }, 2000)
+      })
   }
 )
+
+const handleWhiser = () => {
+  echo.join(`conversation.${conversationStore.selectedConversation?.id}`).whisper('typing', {
+    typing: true
+  })
+}
 
 const handleSendMessage = async () => {
   let data = {
@@ -37,8 +55,14 @@ const handleSendMessage = async () => {
       <Message v-for="msg in store.messages" :key="msg.id" :message="msg" />
     </ul>
   </div>
+  <div v-if="typing" class="text-sm text-gray-500 mt-2">Typing...</div>
   <div class="flex gap-2">
-    <TextInput v-model="message" class="py-4 w-full" placeholder="type here..." />
+    <TextInput
+      @input="handleWhiser"
+      v-model="message"
+      class="py-4 w-full"
+      placeholder="type here..."
+    />
     <button @click="handleSendMessage" class="bg-blue-500 text-white p-2 rounded h-12 mt-4">
       Send
     </button>
