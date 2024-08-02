8c698c0abe2a6bb7e539267b57c4d46553781871
HBilal Khan Yousafzai <hbilal_9@hotmail.com>
2024-08-02 10:17:47 +0500

    Chat layout design created

--- frontent-vue/src/views/HomeView.vue
+++ frontent-vue/src/views/HomeView.vue
@@ -1,7 +1,23 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main>
-    <div class="text-2xl text-red-500">Hello, This is First Page</div>
-  </main>
+  <div class="flex gap-2 w-full px-10 h-screen">
+    <div class="flex flex-col w-[18rem]">
+      <h1 class="text-2xl font-bold">Conversations</h1>
+      <p class="text-sm text-gray-500">Click on a conversation to see the messages</p>
+      <ul class="space-y-2 mt-4 grow bg-primary-300 px-2 py-4 rounded">
+        <li class="px-2 py-3 rounded bg-gray-200 cursor-pointer hover:bg-primary-200">
+          <span>HBilal Khan</span>
+        </li>
+        <li class="px-2 py-3 rounded bg-gray-200 cursor-pointer hover:bg-primary-200">
+          <span>HBilal Khan</span>
+        </li>
+      </ul>
+    </div>
+    <div class="flex flex-col grow text-center">
+      <h1 class="text-2xl font-bold">Select Conversation To Start Chatting</h1>
+      <p class="text-sm text-gray-500">Click on a conversation to see the messages</p>
+      <div class="bg-gray-200 rounded h-full w-full mt-4">Hello</div>
+    </div>
+  </div>
 </template>
