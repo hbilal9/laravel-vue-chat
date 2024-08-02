f23f3465abf3f3df96b6290ed13d99eafc0e26ea
HBilal Khan Yousafzai <hbilal_9@hotmail.com>
2024-08-02 10:38:43 +0500

    token added to axios client

--- frontent-vue/src/services/http.ts
+++ frontent-vue/src/services/http.ts
@@ -1,10 +1,12 @@
 import axios from 'axios'
 
 export function http() {
+  const token = localStorage.getItem('token') || ''
   return axios.create({
     baseURL: 'http://localhost:8000/api',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
+      Authorization: token ? `Bearer ${token}` : ''
     }
   })
 }
