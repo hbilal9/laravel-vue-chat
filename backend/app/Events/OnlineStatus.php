950209e49ebdf6c8172b1292ed031a5a9ae2758b
HBilal Khan Yousafzai <hbilal_9@hotmail.com>
2024-08-02 14:18:43 +0500

    User OnlineStatus event created

---
+++ backend/app/Events/OnlineStatus.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\User;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class OnlineStatus implements ShouldBroadcastNow
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public $user;
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        // Get all conversations this user is part of
+        $conversations = $this->user->conversations;
+        // Create an array of channels, one for each conversation
+        return $conversations->map(function ($conversation) {
+            return new PrivateChannel('conversation.' . $conversation->id);
+        });
+    }
+
+    public function broadcastWith(): array
+    {
+        return [
+            'user' => $this->user
+        ];
+    }
+}
