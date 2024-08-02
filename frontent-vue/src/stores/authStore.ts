9f3d2ac852acb7cfbdca238faf4e860db2bb5cc3
HBilal Khan Yousafzai <hbilal_9@hotmail.com>
2024-08-02 09:45:47 +0500

    Login store method created

--- frontent-vue/src/stores/authStore.ts
+++ frontent-vue/src/stores/authStore.ts
@@ -1,3 +1,4 @@
+import { http } from '@/services/http'
 import type { IUser } from '@/types/user'
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
@@ -6,8 +7,20 @@ export const useAuthStore = defineStore('authStore', () => {
   const user = ref<IUser | null>(null)
   const isLoggedIn = ref<boolean>(false)
 
+  const login = async (data: { email: string; password: string }) => {
+    try {
+      const response = await http().post('/login', data)
+      user.value = response.data.user
+      isLoggedIn.value = true
+      localStorage.setItem('token', response.data.access_token)
+    } catch (error: any) {
+      console.log(error)
+    }
+  }
+
   return {
     user,
-    isLoggedIn
+    isLoggedIn,
+    login
   }
 })
