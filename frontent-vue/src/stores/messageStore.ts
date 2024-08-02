0f7f85f31b521b16d248edeafb9ea5106e09b883
HBilal Khan Yousafzai <hbilal_9@hotmail.com>
2024-08-02 16:36:54 +0500

    messageStore fetch messages api method created

--- frontent-vue/src/stores/messageStore.ts
+++ frontent-vue/src/stores/messageStore.ts
@@ -1,8 +1,19 @@
+import { http } from '@/services/http'
+import type { IMessage } from '@/types/conversation'
 import { defineStore } from 'pinia'
 
 export const useMessageStore = defineStore('messageStore', {
   state: () => ({
-    messages: []
+    messages: [] as IMessage[]
   }),
-  actions: {}
+  actions: {
+    async fetchMessages(conversationId: number) {
+      try {
+        const response = await http().get(`/users/conversations/${conversationId}/messages`)
+        this.messages = response.data
+      } catch (error: any) {
+        console.log(error)
+      }
+    }
+  }
 })
